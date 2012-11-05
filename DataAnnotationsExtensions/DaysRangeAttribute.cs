ddac86a1c7b00b967672e44227c06d692b140537
jinlook <blogjinlook@gmail.com>
2012-11-05 14:51:59 +0800

    Add DaysRangeAttribute and YearsRangeAttribute

---
+++ DataAnnotationsExtensions/DaysRangeAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using DataAnnotationsExtensions.Resources;
+
+namespace DataAnnotationsExtensions
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class DaysRangeAttribute : RangeAttribute
+    {
+        public DaysRangeAttribute(int LessDays, int GreaterDays)
+            : base(
+        typeof(DateTime),
+        DateTime.Now.AddDays(LessDays).ToString("yyyy-MM-dd 00:00:00.000"),            //The min should be yyyy-MM-dd 00:00:00.000
+        DateTime.Now.AddDays(GreaterDays).ToString("yyyy-MM-dd 23:59:59.999")          //The max should be yyyy-MM-dd 23:59:59.999
+    )
+        { }
+    }
+}
---
+++ DataAnnotationsExtensions/YearsRangeAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using DataAnnotationsExtensions.Resources;
+
+namespace DataAnnotationsExtensions
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class YearsRangeAttribute : RangeAttribute
+    {
+        public YearsRangeAttribute(int LessYears, int GreaterYears)
+            : base(
+        typeof(DateTime),
+        DateTime.Now.AddYears(LessYears).ToString("yyyy-01-01 00:00:00.000"),      //The min should be yyyy-01-01 00:00:00.000
+        DateTime.Now.AddYears(GreaterYears).ToString("yyyy-12-31 23:59:59.999")    //The max should be yyyy-12-31 23:59:59.999
+    )
+        { }
+    }
+}
